b726454ae2b9092242e5d11f19b04715e2f2c57e
Abhijithu5 <jithu19981029@gmail.com>
2023-03-04 05:41:40 +1200

    clickfun ex1.html

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function myloadfunction() {
+    var element = document.getElementById('pageheading');
+    var elements = document.getElementById('pagecontent');
+    element.firstChild.nodeValue = 'new heading'; 
+    elements.firstChild.nodeValue = 'Booo';  
+};
+document.addEventListener('click',myloadfunction);
+
+
 function print5(){
     console.log(1);
     console.log(2);
